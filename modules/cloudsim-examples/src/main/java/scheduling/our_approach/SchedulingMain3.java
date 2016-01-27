ad64e893832d820d177127b3ea19a867469fd104
outDream <2428091608@qq.com>
2016-01-27 21:55:18 +0800

    nothing

--- modules/cloudsim-examples/src/main/java/scheduling/our_approach/SchedulingMain3.java
+++ modules/cloudsim-examples/src/main/java/scheduling/our_approach/SchedulingMain3.java
@@ -36,39 +36,39 @@ public class SchedulingMain3 {
 			int numProcessor = SchedulingConstants.NUMBER_OF_PROCESSORS;
 			List<Cloudlet> tempCloudlets = null;
 			int iCnt = 1;
-//			while(iCnt <= SchedulingConstants.NUMBER_OF_ITERATIONS) {
-//				SchedulingHelper.outputToResultFile(originOutput, result_output, "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"+iCnt+"th OVERALL　ITERATION$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-//				//some processor along to search more search space
-//				processors = new OverallExchangeProcessor[numProcessor];
-//				for(int i=0; i<numProcessor; i++) {
-//					processors[i] = new OverallExchangeProcessor(i+1, mapping, hostList, cloudletList, vmList, result_output, mediate_result_output, originOutput);
-//					processors[i].run();
-//				}
-//				
-//				//select the max fitness processor
-//				double max_fitness = Double.MIN_VALUE;
-//				int max_num = -1;
-//				for(int i=0; i<numProcessor; i++) {
-//					if(max_fitness<processors[i].getLocalMapping().getFitness()){
-//						max_fitness = processors[i].getLocalMapping().getFitness();
-//						max_num = i;
-//					}
-//				}
-//				//update mapping
-//				int hostId;
-//				for(int i=0; i<mapping.getNumVms(); i++) {
-//					hostId = processors[max_num].getLocalMapping().getHostOfVm(i);
-//					mapping.setHostOfVm(i, hostId);
-//				}
-//				
-//				tempCloudlets=SchedulingHelper.getCopyOfCloudlets(cloudletList);
-//				SchedulingHelper.simulation(tempCloudlets, hostList, vmList, mapping, SchedulingConstants.our_normal_vmAllocationPolicy);
-//				SchedulingHelper.outputResultToResultFile(iCnt++ +"th iteration ", originOutput, result_output, mapping, 1);
-//			}
-//			
-//			tempCloudlets=SchedulingHelper.getCopyOfCloudlets(cloudletList);
-//			SchedulingHelper.simulation(tempCloudlets, hostList, vmList, mapping, SchedulingConstants.our_normal_vmAllocationPolicy);
-//			SchedulingHelper.outputResultToResultFile("Final", originOutput, result_output, mapping, 1);
+			while(iCnt <= SchedulingConstants.NUMBER_OF_ITERATIONS) {
+				SchedulingHelper.outputToResultFile(originOutput, result_output, "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"+iCnt+"th OVERALL　ITERATION$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+				//some processor along to search more search space
+				processors = new OverallExchangeProcessor[numProcessor];
+				for(int i=0; i<numProcessor; i++) {
+					processors[i] = new OverallExchangeProcessor(i+1, mapping, hostList, cloudletList, vmList, result_output, mediate_result_output, originOutput);
+					processors[i].run();
+				}
+				
+				//select the max fitness processor
+				double max_fitness = Double.MIN_VALUE;
+				int max_num = -1;
+				for(int i=0; i<numProcessor; i++) {
+					if(max_fitness<processors[i].getLocalMapping().getFitness()){
+						max_fitness = processors[i].getLocalMapping().getFitness();
+						max_num = i;
+					}
+				}
+				//update mapping
+				int hostId;
+				for(int i=0; i<mapping.getNumVms(); i++) {
+					hostId = processors[max_num].getLocalMapping().getHostOfVm(i);
+					mapping.setHostOfVm(i, hostId);
+				}
+				
+				tempCloudlets=SchedulingHelper.getCopyOfCloudlets(cloudletList);
+				SchedulingHelper.simulation(tempCloudlets, hostList, vmList, mapping, SchedulingConstants.our_normal_vmAllocationPolicy);
+				SchedulingHelper.outputResultToResultFile(iCnt++ +"th iteration ", originOutput, result_output, mapping, 1);
+			}
+			
+			tempCloudlets=SchedulingHelper.getCopyOfCloudlets(cloudletList);
+			SchedulingHelper.simulation(tempCloudlets, hostList, vmList, mapping, SchedulingConstants.our_normal_vmAllocationPolicy);
+			SchedulingHelper.outputResultToResultFile("Final", originOutput, result_output, mapping, 1);
 			
 			originOutput.close();
 			File file = new File(SchedulingConstants.OutputFolder + "/" +SchedulingConstants.our_log_file+num_all_cloudlets+".txt");
